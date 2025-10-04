f5588e0f5904f35fa65d0328504b41a654302760
Jan-Phillip Oesterling <jappi2000@ewetel.net>
2025-10-04 21:35:27 +0200

    Update HookJobProcess.php

--- src/HookJobProcess.php
+++ src/HookJobProcess.php
@@ -25,7 +25,7 @@ class HookJobProcess
                 ->withHeaders($webhookClient->header)
                 ->useSecret(config('webhook-server.webhook_secret'))
                 ->payload([$this->payload($this->model, $this->event, $this->module, $webhookClient->data_option)])
-                ->dispatchSync();
+                ->dispatch();
         }
     }
 }
